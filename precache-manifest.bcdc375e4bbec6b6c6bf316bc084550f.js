823ccdb2c35f03c3663d02f552f4eebe719b9c7f
chelsejw <chelsejw@gmail.com>
2020-08-30 21:28:25 +0800

    Updates

---
+++ precache-manifest.bcdc375e4bbec6b6c6bf316bc084550f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09417c5fb52eeca6badfeef95efa1ff2",
+    "url": "/techsg/index.html"
+  },
+  {
+    "revision": "bb1c0add6740ed840d25",
+    "url": "/techsg/static/css/main.d5ce62d2.chunk.css"
+  },
+  {
+    "revision": "ab1b4c0f6d6d935b3c76",
+    "url": "/techsg/static/js/2.5556d30a.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/techsg/static/js/2.5556d30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1c0add6740ed840d25",
+    "url": "/techsg/static/js/main.b453ebca.chunk.js"
+  },
+  {
+    "revision": "51fd2078179b4ca0a120",
+    "url": "/techsg/static/js/runtime-main.a894e14b.js"
+  }
+]);
